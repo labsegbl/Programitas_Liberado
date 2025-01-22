13924fb47f0950f7ba3ee53d741deacac7b5706c
Georky <georky27condoy@gmail.com>
2025-01-21 22:58:11 -0500

    updateApi2

--- programitas/settings.py
+++ programitas/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'programitas.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}"""
+}
 
 
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'db_ciberbl',
@@ -98,7 +98,7 @@ DATABASES = {
         'HOST': 'dpg-ctuo0nggph6c73eshja0-a.oregon-postgres.render.com',
         'PORT': '5432',
     }
-}
+}"""
 
 if config("IN_PRODUCTION", cast=bool):
     DATABASES["default"] = dj_database_url.parse(config("DATABASE_URL"))
